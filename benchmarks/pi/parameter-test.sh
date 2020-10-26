1e7e1e99eae0fd300756687bea8fe9f7d0706e04
davida326 <davidvass97@gmail.com>
2020-10-26 17:31:59 +0100

    Json output

--- benchmarks/pi/parameter-test.sh
+++ benchmarks/pi/parameter-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Készítette Vass Dávid Attila
 sysctl kernel.sched_tunable_scaling=0
-#Ezt  a tunable_scaling paramétert, 0-ra kell hogy állítsam mivel ez a beállított értékeimen még módosíthat.
+#Ezt  a tunable_scaling paramétert, 0-ra kell hogy állítsam mivel ez a beállított értékeimen még módosítani.
 #Emiatt a teszt nem lenne eredményes.
 #A programban kezdetben négy részre vannak szétszedve a beállítási paraméterek és minden beállítással 5 mintát veszek.
 #Ebből remélhetőleg lehet majd látni a változásokat.
@@ -15,6 +15,7 @@ prio=0;
 setPrio=0;
 #Két prioritás változót használok mivel ezt nem állíthatom sysctl segítségével.
 #Az első változó a beállítások iterációjához szükséges, a setPrio pedig a konkrét prioritással futtatja a programot.
+echo '[' &>>ertekek.log
 setLatency() {
     case $1 in
         0)
@@ -69,42 +70,46 @@ setWakeup_gran() {
             echo "ERROR!setWakeup_gran" >> log
     esac
 }
-for latency in {0..3}
-do
-    for min_gran in {0..3}
-    do
-        for wakeup_gran in {0..3}
-        do
-            for prio in {0..3}
-            do
+ for latency in {0..3}
+ do
+     for min_gran in {0..3}
+     do
+         for wakeup_gran in {0..3}
+         do
+             for prio in {0..3}
+             do
                 echo $latency:$min_gran:$wakeup_gran:$prio
-                echo "************************************************" >> log
                 setLatency $latency
                 setMin_gran $min_gran
                 setWakeup_gran $wakeup_gran
-                echo $latency $min_gran $wakeup_gran $prio >> log
-                case $prio in
-                    0)
-                        setPrio=-20
-                        ;;
-                    1)
-                        setPrio=-6
-                        ;;
-                    2)
-                        setPrio=6
-                        ;;
-                    3)
-                        setPrio=19
-                        ;;
-                    *)
-                        echo "ERROR!setPrio" >> log
-                esac
-                { time nice -$setPrio ./sample-program; } &>>log 
-                { time nice -$setPrio ./sample-program; } &>>log 
-                { time nice -$setPrio ./sample-program; } &>>log 
-                { time nice -$setPrio ./sample-program; } &>>log 
-                { time nice -$setPrio ./sample-program; } &>>log 
-            done
+                echo '{"latency":"'$latency'", "min_gran":"'$min_gran'", "wakeup_gran":"'$wakeup_gran'","prio:":"'$prio'","run":[{"0":"' &>>ertekek.log
+                    case $prio in
+                        0)
+                            setPrio=-20
+                            ;;
+                        1)
+                            setPrio=-6
+                            ;;
+                        2)
+                            setPrio=6
+                            ;;
+                        3)
+                            setPrio=19
+                            ;;
+                        *)
+                            echo "ERROR!setPrio" >> ertekek.log
+                    esac
+                    { /usr/bin/time -f "%e" nice -$setPrio ./sample-program; } &>>ertekek.log
+                    echo '","1":"' &>>ertekek.log 
+                    { /usr/bin/time -f "%e" nice -$setPrio ./sample-program; } &>>ertekek.log
+                    echo '","2":"' &>>ertekek.log 
+                    { /usr/bin/time -f "%e" nice -$setPrio ./sample-program; } &>>ertekek.log 
+                    echo '","3":"' &>>ertekek.log
+                    { /usr/bin/time -f "%e" nice -$setPrio ./sample-program; } &>>ertekek.log 
+                    echo '","4":"' &>>ertekek.log
+                    { /usr/bin/time -f "%e" nice -$setPrio ./sample-program; } &>>ertekek.log 
+                    echo '"}]},' &>>ertekek.log
+   		    done
  
         done
 
@@ -112,3 +117,4 @@ do
 
 done
 
+echo ']' &>>ertekek.log
