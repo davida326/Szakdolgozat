33d7d7bf6cd9efb241e135f4fb551be9a265c2ce
Vass Dávid Attila <55162386+davida326@users.noreply.github.com>
2021-02-27 14:19:23 +0100

    Update main.c

--- parameter-test-program/parameter-test/src/main.c
+++ parameter-test-program/parameter-test/src/main.c
@@ -10,7 +10,7 @@
                             Ha nem adunk meg számot, csak Enter-t nyomunk, 5 mintát fog venni tesztenként.
                         -parameter split megadása, itt azt adjuk meg hogy a tesztelni kívánt paraméterek intervallumát, hány részre szedjük szét
                             Egyetlen enter megnyomása esetén, alapbeállítás 5.
-                        -Amennyiben létezik a teszthez, a szükséges konfigurációs beállítások megadása számokkal (1-N)
+                        -Amennyiben létezik a teszthez, a szükséges konfigurációs beállítások megadása számokkal (0-N)
                         -A program futása során, nincs lehetőség részeredmények mentésére.
                         -Kimenet: egyetlen .json fájl a ./logs jegyzékben
 
